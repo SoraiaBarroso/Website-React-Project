edd245b5f0b2797d9ccf4e871ac19af3a9f6d742
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-09-17 09:48:28 +0000

    subscribe

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,7 +1,15 @@
+import "./Subscribe.css"
+
 const Subscribe = () => {
     return(
         <section id="subscribe">
-
+            <div className="container susbcribe">
+                <h1>SUBSCRIBE</h1>
+                <form>
+                    <input type="email" placeholder="Your Email"></input>
+                    <button onClick={(e) => {e.preventDefault();}}>Send</button>
+                </form>
+            </div>
         </section>
     )
 }
