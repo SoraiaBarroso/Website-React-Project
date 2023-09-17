572614dacdffd63ab4ca8a0a1360b6f0df808c0e
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-09-17 17:26:00 +0000

    sbs

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,14 +1,29 @@
 import "./Subscribe.css"
 
+import {AiFillAccountBook, AiOutlineTwitter} from "react-icons/ai"
+import {AiOutlineInstagram} from "react-icons/ai"
+import {BiLogoFacebookCircle} from "react-icons/bi"
+
 const Subscribe = () => {
     return(
         <section id="subscribe">
             <div className="container susbcribe">
-                <h1>SUBSCRIBE</h1>
+                <h1>SUBSCRIBE NOW!</h1>
                 <form>
-                    <input type="email" placeholder="Your Email"></input>
+                    <input type="text" placeholder="techx@hotmail.com"></input>
                     <button onClick={(e) => {e.preventDefault();}}>Send</button>
                 </form>
+            </div>
+            <div className="social-icon">
+                <div className="icon-sb">
+                    <AiOutlineInstagram size={25} color="white"/>
+                </div>
+                <div className="icon-sb">
+                    <AiOutlineTwitter size={25} color="white" />
+                </div>
+                <div className="icon-sb">
+                    <BiLogoFacebookCircle size={25} color="white"/>
+                </div>
             </div>
         </section>
     )
