ed15ba21705eabe5e513c8704fdd02bd992dcb6c
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-09-18 14:09:44 +0000

    email submition window

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,17 +1,32 @@
 import "./Subscribe.css"
 
-import {AiFillAccountBook, AiOutlineTwitter} from "react-icons/ai"
+import EmailImg from "../../assets/mail.svg"
+import {AiOutlineClose} from "react-icons/ai"
+import {AiOutlineTwitter} from "react-icons/ai"
 import {AiOutlineInstagram} from "react-icons/ai"
 import {BiLogoFacebookCircle} from "react-icons/bi"
+import { useState } from "react"
 
 const Subscribe = () => {
+
+    const [showWindow, setShowWindow] = useState(false);
+
+    function HandleBtn(e) {
+        setShowWindow(true);
+        e.preventDefault();
+    }   
+
+    function CloseWindow() {
+        setShowWindow(false);
+    }
+
     return(
         <section id="subscribe">
             <div className="container susbcribe">
                 <h1>SUBSCRIBE NOW!</h1>
                 <form>
-                    <input type="text" placeholder="techx@hotmail.com"></input>
-                    <button onClick={(e) => {e.preventDefault();}}>Send</button>
+                    <input type="email" name="email" placeholder="techx@hotmail.com" required/>
+                    <button onClick={HandleBtn}>Send</button>
                 </form>
             </div>
             <div className="social-icon">
@@ -25,6 +40,22 @@ const Subscribe = () => {
                     <BiLogoFacebookCircle size={25} color="white"/>
                 </div>
             </div>
+            {
+                showWindow === true ? 
+                <div className="subs-panel">
+                    <div className="window-subs">
+                        <div className="img-email">
+                            <img src={EmailImg} alt="mail img"></img>
+                        </div>
+                        <div className="text-email">
+                            <h1>Thank you!</h1>
+                            <p>Your will receive emails with the latest news about our project</p>
+                        </div>
+                        <AiOutlineClose onClick={CloseWindow} className="close-icon" size={25} color="rgba(19, 55, 113, 0.957)"/>
+                    </div>
+                </div> 
+                : <div></div>
+            }
         </section>
     )
 }
