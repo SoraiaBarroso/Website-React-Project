cd919cefda17311daa5e4027aad457d6493a2d99
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-09-20 16:20:05 +0000

    faq

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,6 +1,8 @@
 import "./Faq.css"
 
 import faqImg from "../../assets/faq.svg"
+import rocket from "../../assets/rocket.png"
+
 
 const Faq = () => {
   return (
@@ -8,24 +10,24 @@ const Faq = () => {
         <div className="container faq"> 
             <div className="container-grid">
                 <div className="grid-item">
-                    <img alt="img"></img>
-                    <h1>getting started</h1>
-                    <p>get started with....</p>
+                    <img src={rocket} alt="img"></img>
+                    <h1>Getting started</h1>
+                    <p>Te damos la bienvenida a Slack, nos alegramos de verte. Y ahora, manos a la obra.</p>
                 </div>
                 <div className="grid-item">
-                    <img alt="img"></img>
-                    <h1>getting started</h1>
-                    <p>get started with....</p>
+                    <img src={rocket} alt="img"></img>
+                    <h1>Getting started</h1>
+                    <p>Te damos la bienvenida a Slack, nos alegramos de verte. Y ahora, manos a la obra.</p>
                 </div>
                 <div className="grid-item">
-                    <img alt="img"></img>
-                    <h1>getting started</h1>
-                    <p>get started with....</p>
+                    <img src={rocket} alt="img"></img>
+                    <h1>Getting started</h1>
+                    <p>Te damos la bienvenida a Slack, nos alegramos de verte. Y ahora, manos a la obra.</p>
                 </div>
                 <div className="grid-item">
-                    <img alt="img"></img>
-                    <h1>getting started</h1>
-                    <p>get started with....</p>
+                    <img src={rocket} alt="img"></img>
+                    <h1>Getting started</h1>
+                    <p>Te damos la bienvenida a Slack, nos alegramos de verte. Y ahora, manos a la obra.</p>
                 </div>
             </div>
             <div className="container-right">
