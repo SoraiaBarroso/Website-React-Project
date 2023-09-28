015f234a0e95e6a2d6ceeb5c42808a98f180421e
Soraia <115974717+SoraiaBarroso@users.noreply.github.com>
2023-09-28 12:13:58 +0000

    analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { inject } from '@vercel/analytics';
+
+// Call the inject function to add the tracking script to your app.
+inject();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
